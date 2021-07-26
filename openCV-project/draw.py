3578dcf51ff173074a073111bbc58fb1648a1546
nagachi <rafly.rigannagachi@gmail.com>
2021-07-26 19:01:20 +0700

    openCV: update python convention

--- openCV-project/draw.py
+++ openCV-project/draw.py
@@ -9,9 +9,11 @@ blank = np.zeros((500, 500, 3), dtype='uint8')
 blank[100:200, 250:] = 105, 50, 100
 
 cv2.rectangle(
-    blank, (0, 0), (blank.shape[1]//2, blank.shape[0]//2), (255, 0, 0), thickness=-1)
+    blank, (0, 0), (blank.shape[1]//2, blank.shape[0]//2),
+    (255, 0, 0), thickness=-1)
 cv2.circle(
-    blank, (blank.shape[1]//2, blank.shape[0]//2), 40, (0, 0, 255), thickness=-1)
+    blank, (blank.shape[1]//2, blank.shape[0]//2),
+    40, (0, 0, 255), thickness=-1)
 cv2.line(blank, (200, 150),
          (blank.shape[1]//4, blank.shape[0]//2), (255, 255, 0), thickness=5)
 cv2.putText(blank, 'Hello, Nagachi!', (100, 400),
