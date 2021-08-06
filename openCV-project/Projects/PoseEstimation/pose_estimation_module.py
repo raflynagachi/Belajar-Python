a4ee8b80a64f9530effceec29ea17be777079b7a
nagachi <rafly.rigannagachi@gmail.com>
2021-08-06 21:48:02 +0700

    add find angle method

--- openCV-project/Projects/PoseEstimation/pose_estimation_module.py
+++ openCV-project/Projects/PoseEstimation/pose_estimation_module.py
@@ -1,6 +1,7 @@
 import cv2
 import mediapipe as mp
 import time
+import math
 
 
 class poseDetector():
@@ -24,19 +25,47 @@ class poseDetector():
             if draw:
                 self.mpDraw.draw_landmarks(img, self.results.pose_landmarks,
                                            self.mpPose.POSE_CONNECTIONS)
+        return img
 
     def findPosition(self, img, draw=True):
-        lmList = []
+        self.lmList = []
         if self.results.pose_landmarks:
             h, w, c = img.shape
             for id, lm in enumerate(self.results.pose_landmarks.landmark):
                 cx, cy = int(lm.x*w), int(lm.y*h)
                 # print([id, cx, cy])
-                lmList.append([id, cx, cy])
+                self.lmList.append([id, cx, cy])
                 if draw:
                     cv2.putText(img, str(id), (cx+5, cy+5),
                                 cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.7, (0, 255, 0), 1)
-        return lmList
+        return self.lmList
+
+    def findAngle(self, img, p1, p2, p3, draw=True):
+        x1, y1 = self.lmList[p1][1:]
+        x2, y2 = self.lmList[p2][1:]
+        x3, y3 = self.lmList[p3][1:]
+
+        # Calculate angle
+        angle = math.degrees(math.atan2(y3-y2, x3-x2)-math.atan2(y1-y2, x1-x2))
+        if angle < 0:
+            angle += 360
+        # print(angle)
+
+        if draw:
+            cv2.line(img, (x1, y1), (x2, y2), (255, 255, 255), 3)
+            cv2.line(img, (x3, y3), (x2, y2), (255, 255, 255), 3)
+
+            cv2.circle(img, (x1, y1), 10, (0, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), 10, (0, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x3, y3), 10, (0, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x1, y1), 15, (0, 0, 255), 2)
+            cv2.circle(img, (x2, y2), 15, (0, 0, 255), 2)
+            cv2.circle(img, (x3, y3), 15, (0, 0, 255), 2)
+
+            cv2.putText(img, f'{int(angle)}', (x2-30, y2-20),
+                        cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 255), 2)
+
+        return angle
 
 
 def main():
