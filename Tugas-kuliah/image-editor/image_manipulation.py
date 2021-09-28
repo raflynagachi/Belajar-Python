230f839ca29db5ef06fe00573c14ea8d0a14434d
nagachi <rafly.rigannagachi@gmail.com>
2021-09-28 18:43:54 +0700

    add arithmetic, boolean, and geometri operation

--- Tugas-kuliah/image-editor/image_manipulation.py
+++ Tugas-kuliah/image-editor/image_manipulation.py
@@ -1,13 +1,7 @@
-from numpy import dtype
 import numpy as np
 
 
 class image_manipulation:
-    # def __init__(self, image):
-    #     self.image = image
-
-    # def set_image(self, image):
-    #     self.image = image
 
     def biner(self, citra, threshold, M, N):
         # Membuat citra biner dari citra A berdasarkan nilai ambang
@@ -57,3 +51,84 @@ class image_manipulation:
                     else:
                         citra_bright[i][j] = temp
         return citra_bright
+
+    def addition(self, citra_a, citra_b, M, N):
+        # Menjumlahkan dua buah citra A dan B menjadi citra baru, C.
+        # Citra A, B, dan C masing-masing berukuran M x N.
+        citra_add = np.zeros((M, N), dtype=int)
+        temp = 0
+        for i in range(M):
+            for j in range(N):
+                temp = citra_a[i][j]+citra_b[i][j]
+                if temp > 255:
+                    citra_add[i][j] = 0
+                else:
+                    citra_add[i][j] = temp
+        return citra_add
+
+    def substraction(self, citra_a, citra_b, M, N):
+        # Mengurangkan dua buah citra A dan B menajdi citra baru, C.
+        # Citra A, B, dan C berukuran M x N.
+        citra_sub = np.zeros((M, N), dtype=int)
+        for i in range(M):
+            for j in range(N):
+                citra_sub[i][j] = citra_a[i][j] - citra_b[i][j]
+                if citra_sub[i][j] != 0:
+                    citra_sub[i][j] = 255
+        return citra_sub
+
+    def multiplication(self, citra_a, citra_b, M, N):
+        # Mengalikan citra A dengan citra B menjadi citra C.
+        # Citra A, matriks B, dan hasil perkalian C berukuran M x N.
+        citra_mult = np.zeros((M, N), dtype=int)
+        for i in range(M):
+            for j in range(N):
+                for k in range(N):
+                    citra_mult[i][j] = citra_a[i][j] * citra_b[i][j]
+                    # clipping
+                    if citra_mult[i][j] < 0:
+                        citra_mult[i][j] = 0
+                    elif citra_mult[i][j] > 255:
+                        citra_mult[i][j] = 255
+        return citra_mult
+
+    def not_boolean(self, citra_biner_a, M, N):
+        # Membuat citra komplemen dari citra biner A.
+        # Komplemennya disimpan di dalam B. Ukuran citra A
+        # adalah M x N.
+        citra_not_boolean = np.zeros((M, N), dtype=int)
+        for i in range(M):
+            for j in range(N):
+                citra_not_boolean[i][j] = not citra_biner_a[i][j]
+        return citra_not_boolean
+
+    def translation(self, citra, M, N, b, a):
+        # Mentranslasi citra A sejauh a, b menjadi citra B. Ukuran citra M x N.
+        citra_translation = np.zeros((M, N), dtype=int)
+        for i in range(M):
+            for j in range(N):
+                if ((i+a) < M) and ((j+b) < N) and ((i+a) >= 0) and ((j+b) >= 0):
+                    citra_translation[i][j] = citra[i+a][j+b]
+        return citra_translation
+
+    def rotation90CCW(self, citra, M, N):
+        # Rotasi citra A sejauh 90 berlawanan arah jarum jam (CCW = Clock Counterwise).
+        # Ukuran citra adalah M x N. Hasil rotasi disimpan di dalam citra B.
+        citra_rotated = np.zeros((M, N), dtype=int)
+        for i in range(M):
+            k = N-1
+            for j in range(N):
+                citra_rotated[k][i] = citra[i][j]
+                k -= 1
+        return citra_rotated
+
+    def rotation90CW(self, citra, M, N):
+        # Rotasi citra A sejauh 90 searah jarum jam (CW = Clockwise).
+        # Ukuran citra adalah M x N. Hasil rotasi disimpan di dalam citra B.
+        citra_rotated = np.zeros((M, N), dtype=int)
+        k = N-1
+        for i in range(M):
+            for j in range(N):
+                citra_rotated[j][k] = citra[i][j]
+            k -= 1
+        return citra_rotated
--- Tugas-kuliah/image-editor/main.py
+++ Tugas-kuliah/image-editor/main.py
@@ -1,9 +1,9 @@
 # import required modules
 import tkinter as tk
 import numpy as np
-from tkinter import StringVar, Variable, ttk, Tk, filedialog
+from tkinter import ttk, Tk, filedialog
 from tkinter.filedialog import askopenfilename, asksaveasfilename
-from PIL import Image, ImageTk, ImageFilter, ImageEnhance, ImageOps
+from PIL import Image, ImageTk
 import os
 
 from numpy.lib.type_check import imag
@@ -12,22 +12,37 @@ import image_manipulation as img_m
 # contrast border thumbnail
 root = Tk()
 root.title("Simple Photo Editor")
-root.geometry("640x640")
+root.geometry("940x640")
+root.resizable(False, False)
 
 
-def selected():
-    global canvas2, img, img_array, img_path, M, N
-    img_path = filedialog.askopenfilename(initialdir=os.getcwd())
-    img = Image.open(img_path)
-    img.thumbnail((350, 350))
-    img_array = np.array(img)
-
-    img1 = ImageTk.PhotoImage(img)
-    canvas2.create_image(300, 210, image=img1)
-    canvas2.image = img1
-    M = img1.height()
-    N = img1.width()
-    print(img_array)
+def selected(image_idx=1):
+    global canvas2, img, img_array, img_path, M, N, img_array2
+    if image_idx == 1:
+        img_path = filedialog.askopenfilename(initialdir=os.getcwd())
+        img = Image.open(img_path)
+        img.thumbnail((250, 250))
+        img_array = np.array(img)
+        img1 = ImageTk.PhotoImage(img)
+        canvas2.create_image(300, 210, image=img1)
+        canvas2.image = img1
+        M = img1.height()
+        N = img1.width()
+    else:
+        img_path2 = filedialog.askopenfilename(initialdir=os.getcwd())
+        imgg = Image.open(img_path2)
+        imgg.thumbnail((250, 250))
+        M_imgg = imgg.height
+        N_imgg = imgg.width
+        img_array2 = np.array(imgg)
+        img_array2 = img_mnp.grayscale(img_array2, M_imgg, N_imgg)
+        # img_array2 = img_mnp.biner(img_array2, 180, M_imgg, N_imgg)
+        imgg = Image.fromarray(np.uint8(img_array2))
+        # imgg = imgg.convert('L').point(
+        #     lambda x: 0 if x == 0 else 255, "1")
+        img2 = ImageTk.PhotoImage(imgg.resize((210, 210)))
+        canvas3.create_image(155, 115, image=img2)
+        canvas3.image = img2
     print('Image selected')
 
 
@@ -53,12 +68,18 @@ def save():
         print('Image saved')
 
 
+def save_temp():
+    global img_array, img_result
+    img_array = np.array(img_result)
+    print('Image saved...')
+
+
 def update_image(img_result_arr, binary=False):
     global img_result, canvas2
     img_result = Image.fromarray(np.uint8(img_result_arr))
     if binary:
-        img_result = img_result.convert('L').point(
-            lambda x: 0 if x == 0 else 255, "1")
+        img_result = img_result.point(
+            lambda x: 0 if x == 0 else 255)
     img_result_tk = ImageTk.PhotoImage(
         image=img_result)
     canvas2.create_image(300, 210, image=img_result_tk)
@@ -92,47 +113,159 @@ def negative():
     update_image(img_result_arr)
 
 
+def img_add():
+    global img_mnp, img_array, M, N
+    img_result_arr = img_mnp.addition(img_array, img_array2, M, N)
+    update_image(img_result_arr, True)
+
+
+def img_sub():
+    global img_mnp, img_array, M, N
+    img_result_arr = img_mnp.substraction(img_array, img_array2, M, N)
+    update_image(img_result_arr, True)
+
+
+def img_mult():
+    global img_mnp, img_array, M, N
+    img_result_arr = img_mnp.multiplication(img_array, img_array2, M, N)
+    update_image(img_result_arr, True)
+
+
+def img_not():
+    global img_mnp, img_array, M, N
+    img_result_arr = img_mnp.not_boolean(img_array, M, N)
+    update_image(img_result_arr, True)
+
+
+def img_trans():
+    global img_mnp, img_array, M, N, input_x_trs, input_y_trs
+    img_result_arr = img_mnp.translation(
+        img_array, M, N, int(input_x_trs.get()), int(input_y_trs.get()))
+    update_image(img_result_arr)
+
+
+def img_rotate(event):
+    global img_mnp, img_array, M, N, rotate_combo
+    if str(rotate_combo.get()) == "90CCW":
+        img_result_arr = img_mnp.rotation90CCW(img_array, M, N)
+    else:
+        img_result_arr = img_mnp.rotation90CW(img_array, M, N)
+    update_image(img_result_arr)
+
+
 # Global variable
 img_path = ''
 img = None
 img_array = None
+img_array2 = None
 img_result = None
 M = 0
 N = 0
 
 img_mnp = img_m.image_manipulation()
 
+# ==================Display GUI==================
+# Disclaimer text
+text_content = """
+*Grayscaling before applying other features\n
+*images 2 is for boolean operation\n('NOT' button)\n
+*arithmatic operator using 2 images\n(Violet button)\n
+*use top right green button to update your image"""
+
+text_disc = tk.Text(root, height=18, width=40, highlightthickness=3, padx=4,
+                    highlightbackground='#B33A3A')
+text_disc.place(x=630, y=420)
+text_disc.insert(
+    tk.END, text_content)
+
+# ==================Function button==================
+
 # Brightness
-bright = tk.Label(root, text='Brightness', font=('ariel 12 bold'))
+bright = tk.Label(root, text='Brightness: ', font=('ariel 10 bold'))
 bright.place(x=8, y=8)
 bright_val = tk.IntVar()
 bright_scale = ttk.Scale(root, from_=0, to=255, variable=bright_val,
                          orient=tk.HORIZONTAL, command=brightening)
-bright_scale.place(x=110, y=10)
+bright_scale.place(x=90, y=8)
 
 # Biner
-biner_label = tk.Label(root, text='Biner(0-255)', font=('ariel 12 bold'))
+biner_label = tk.Label(root, text='Biner(0-255): ', font=('ariel 10 bold'))
 biner_label.place(x=250, y=8)
 biner_thresh = tk.IntVar()
 biner_scale = ttk.Scale(root, from_=0, to=255, variable=biner_thresh,
                         orient=tk.HORIZONTAL, command=biner)
-biner_scale.place(x=375, y=10)
-
-# grayscale
-btn_grayscale = tk.Button(root, text="Grayscale", width=8, bg='white', fg='black', border=2,
-                          font=('ariel 12 bold'), relief=tk.GROOVE, command=grayscale)
-btn_grayscale.place(x=520, y=40)
+biner_scale.place(x=345, y=8)
 
 # Negatif
-btn_reset = tk.Button(root, text="Negative", width=8, bg='white', fg='black', border=2,
-                      font=('ariel 12 bold'), relief=tk.GROOVE, command=negative)
+btn_reset = tk.Button(root, text="Negative", width=10, bg='white', fg='black', border=2,
+                      font=('ariel 10 bold'), relief=tk.GROOVE, command=negative)
 btn_reset.place(x=520, y=8)
+
+# grayscale
+btn_grayscale = tk.Button(root, text="Grayscale", width=10, bg='#ff9e42', fg='black', border=2,
+                          font=('ariel 10 bold'), relief=tk.GROOVE, command=grayscale)
+btn_grayscale.place(x=520, y=40)
+
+# addition
+btn_add = tk.Button(root, text="Addition", width=10, bg='#B4A1EE', fg='black', border=2,
+                    font=('ariel 10 bold'), relief=tk.GROOVE, command=img_add)
+btn_add.place(x=420, y=70)
+
+# substraction
+btn_sub = tk.Button(root, text="Substraction", width=10, bg='#B4A1EE', fg='black', border=2,
+                    font=('ariel 10 bold'), relief=tk.GROOVE, command=img_sub)
+btn_sub.place(x=420, y=100)
+
+# multiplication
+btn_mult = tk.Button(root, text="Multiply", width=10, bg='#B4A1EE', fg='black', border=2,
+                     font=('ariel 10 bold'), relief=tk.GROOVE, command=img_mult)
+btn_mult.place(x=520, y=70)
+
+# not operation
+btn_not = tk.Button(root, text="NOT", width=10, bg='#ADD8E6', fg='black', border=2,
+                    font=('ariel 10 bold'), relief=tk.GROOVE, command=img_not)
+btn_not.place(x=520, y=100)
+
+# translation
+input_x_label = tk.Label(root, text='Translation: ', font=('ariel 10 bold'))
+input_x_label.place(x=8, y=40)
+btn_trs = tk.Button(root, text="Translation", width=10, bg='white', fg='black', border=2,
+                    font=('ariel 10 bold'), relief=tk.GROOVE, command=img_trans)
+btn_trs.place(x=100, y=35)
+input_x_label = tk.Label(root, text='input x: ', font=('ariel 10 bold'))
+input_x_label.place(x=8, y=65)
+input_y_label = tk.Label(root, text='input y: ', font=('ariel 10 bold'))
+input_y_label.place(x=8, y=85)
+input_x_trs = tk.Entry(root, border=2)
+input_x_trs.place(x=85, y=65)
+input_x_trs.insert(tk.END, "0")
+input_y_trs = tk.Entry(root, border=2)
+input_y_trs.place(x=85, y=85)
+input_y_trs.insert(tk.END, "0")
+
+# combobox rotate
+rotate = tk.Label(root, text="Rotate:", font=("ariel 10 bold"))
+rotate.place(x=8, y=120)
+values = ["90CW", "90CCW"]
+rotate_combo = ttk.Combobox(root, values=values, font=('ariel 10 bold'))
+rotate_combo.place(x=85, y=120)
+rotate_combo.bind("<<ComboboxSelected>>", img_rotate)
+
+# ==================Display GUI==================
+# ==================Display Image==================
 
 # create canvas to display image
 canvas2 = tk.Canvas(root, width="600", height="420", relief=tk.RIDGE, bd=2)
 canvas2.place(x=15, y=150)
+canvas3 = tk.Canvas(root, width="300", height="220", relief=tk.RIDGE, bd=2)
+canvas3.place(x=630, y=150)
 
 # create buttons
+# Save temp
+btn_save_tmp = tk.Button(root, text="*sv", width=4, bg='#90EE90', fg='black', border=2,
+                         font=('ariel 10 bold'), relief=tk.GROOVE, command=save_temp)
+btn_save_tmp.place(x=860, y=5)
+
 btn_reset = tk.Button(root, text="Reset", width=8, bg='black', fg='red',
                       font=('ariel 15 bold'), relief=tk.GROOVE, command=reset)
 btn_reset.place(x=8, y=595)
@@ -140,6 +273,10 @@ btn_reset.place(x=8, y=595)
 btn_select_image = tk.Button(root, text="Select Image", bg='black', fg='gold',
                              font=('ariel 15 bold'), relief=tk.GROOVE, command=selected)
 btn_select_image.place(x=180, y=595)
+
+btn_select_image2 = tk.Button(root, text="Select Image 2", bg='#ADD8E6', fg='black',
+                              font=('ariel 12 bold'), relief=tk.GROOVE, command=lambda: selected(2))
+btn_select_image2.place(x=630, y=380)
 
 btn_save = tk.Button(root, text="Save", width=8, bg='black', fg='gold',
                      font=('ariel 15 bold'), relief=tk.GROOVE, command=save)
